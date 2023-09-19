c036681ef4a137975a51c1e26888fc2c7a987288
John Owuor <owuor91@gmail.com>
2023-09-19 14:48:32 +0300

    first commit

---
+++ run.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Api
+from db import db
+
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
+
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "postgresql+psycopg2:///kazi"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+app = create_app()
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
